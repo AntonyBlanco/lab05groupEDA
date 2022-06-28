e4dcd55cbdffd0dc588e458f88d50a215d02ce47
Edisson Franklin <69181980+Edisson-Acc-UNSA@users.noreply.github.com>
2022-06-27 23:08:19 -0500

    Estructurando Funcion isBalanced

--- Ejercicio01/CorchetesEquilibrados.java
+++ Ejercicio01/CorchetesEquilibrados.java
@@ -3,4 +3,10 @@ public class CorchetesEquilibrados {
 	public static void main(String[] args) {
 		System.out.println("--------------CORCHETES EQUILIBRADOS--------------");
 	}
+	
+	//funcion que devuelve si una secuencia de corchetes esta balanceada
+	public String isBalanced(String s) {
+		
+		return "SI";
+	}
 }
