d220e0d4b69ace990ec1420bb356fa904fcdf05e
AntonyBlanco <ablancot@unsa.edu.pe>
2022-06-28 20:28:00 -0500

    Optimizacion de insercion del AVL

--- Ejercicio03/AVL.java
+++ Ejercicio03/AVL.java
@@ -9,30 +9,22 @@ public class AVL<T extends Comparable<T>>{
 		return root == null;
 	}
 	public void add(T nuevo){
-		if(this.isEmpty()){
-			this.root = new Node<T>(nuevo);
-		} else {
-			addNew(root, nuevo);
-		}
+		addNew(root, nuevo);
 	}
 	private void addNew(Node<T> actual, T nuevo){
-		int comp = actual.getData().compareTo(nuevo);
-		if(comp < 0){
-			Node<T> actualRight = actual.getRight();
-			if(actualRight == null){
-				actual.setRight(new Node<T>(nuevo));
-			} else {
-				addNew(actualRight, nuevo);
-			}
-		} else if (comp > 0){
-			Node<T> actualLeft = actual.getLeft();
-			if(actualLeft == null){
-				actual.setLeft(new Node<T>(nuevo));
-			} else {
-				addNew(actualLeft, nuevo);
-			}
+		System.out.println("Inserting " + nuevo.toString() + " at " + actual);
+		if(actual == null){
+			actual = new Node<T>(nuevo);
+			System.out.println("Inserted " + nuevo.toString());
 		} else {
-			System.out.println("Elemento repetido");
+			int comp = actual.getData().compareTo(nuevo);
+			if(comp < 0){
+				addNew(actual.getRight(), nuevo);
+			} else if (comp > 0){
+				addNew(actual.getLeft(), nuevo);
+			} else {
+				System.out.println("Elemento repetido");
+			}
 		}
 	}
 
@@ -40,6 +32,9 @@ public class AVL<T extends Comparable<T>>{
 		return inOrder(root);
 	}
 	private String inOrder(Node<T> nodo){
+		if(root == null){
+			return "";
+		}
 		String str = "";
 		Node<T> aux = nodo.getLeft();
 		if(aux != null){
