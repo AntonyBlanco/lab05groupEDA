f51bac8f4e814e619457c9f4ddef1c0b526a6dd9
AntonyBlanco <ablancot@unsa.edu.pe>
2022-06-29 09:39:24 -0500

    Balanceo a la izquierda, simple y doble, en la clase AVL

--- Ejercicio03/AVL.java
+++ Ejercicio03/AVL.java
@@ -1,5 +1,6 @@
 public class AVL<T extends Comparable<T>>{
 	private Node<T> root;
+	private boolean height;
 
 	public AVL(){
 		root = null;
@@ -9,23 +10,88 @@ public class AVL<T extends Comparable<T>>{
 		return root == null;
 	}
 	public void add(T nuevo){
+		height = false;
 		root = addNew(root, nuevo);
 	}
 	private Node<T> addNew(Node<T> actual, T nuevo){
 		System.out.println("Inserting " + nuevo.toString() + " at " + actual);
 		if(actual == null){
+			height = true;
 			actual = new Node<T>(nuevo);
 			System.out.println("Inserted " + nuevo.toString());
 		} else {
 			int comp = actual.getData().compareTo(nuevo);
 			if(comp < 0){
 				actual.setRight(addNew(actual.getRight(), nuevo));
+				if(height){
+					switch(actual.getBalanceFactor()){
+						case -1:
+							actual.setBalanceFactor(0);
+							height = false;
+							break;
+						case 0:
+							actual.setBalanceFactor(1);
+							height = true;
+							break;
+						case 1:
+							//res.balanceFactor = 2;
+							actual = balanceLeft(actual);
+							height = false;
+							break;
+					}
+				}
 			} else if (comp > 0){
 				actual.setLeft(addNew(actual.getLeft(), nuevo));
 			} else {
 				System.out.println("Elemento repetido");
 			}
 		}
+		return actual;
+	}
+	private Node<T> balanceLeft(Node<T> actual){
+		Node<T> hijo = actual.getRight();
+		switch(hijo.getBalanceFactor()){
+			case 1:
+				actual.setBalanceFactor(0);
+				hijo.setBalanceFactor(0);
+				actual = rotacionSimpIzq(actual);
+				break;
+			case -1:
+				Node<T> nieto = hijo.getLeft();
+				switch(nieto.getBalanceFactor()){
+					case -1:
+						actual.setBalanceFactor(0);
+						hijo.setBalanceFactor(-1);
+						break;
+					case 0:
+						actual.setBalanceFactor(0);
+						hijo.setBalanceFactor(0);
+						break;
+					case 1:
+						actual.setBalanceFactor(1);
+						hijo.setBalanceFactor(0);
+						break;
+				}
+				nieto.setBalanceFactor(0);
+				actual.setRight(rotacionSimpDer(hijo));
+				actual = rotacionSimpIzq(actual);
+				break;
+		}
+		return actual;
+	}
+
+	private Node<T> rotacionSimpIzq(Node<T> actual){
+		Node<T> hijo = actual.getRight();
+		actual.setRight(hijo.getLeft());
+		hijo.setLeft(actual);
+		actual = hijo;
+		return actual;
+	}
+	private Node<T> rotacionSimpDer(Node<T> actual){
+		Node<T> hijo = actual.getLeft();
+		actual.setLeft(hijo.getRight());
+		hijo.setRight(actual);
+		actual = hijo;
 		return actual;
 	}
 
